51339f2648e8ee895d0fda18a1b56ecaa3e7e007
ethxn-frs <ethanfrancois0@gmail.com>
2022-11-18 10:00:22 +0100

    poubelle

--- GestionCommerciale/DeclicInfoGUI/Program.cs
+++ GestionCommerciale/DeclicInfoGUI/Program.cs
@@ -16,7 +16,7 @@ namespace DeclicInfoGUI
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new frmMenu());
+            Application.Run(new frmAuthentification());
         }
     }
 }
