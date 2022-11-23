7391c9c090ac753042ed7fd83b4a994cb68c002e
ethxn-frs <ethanfrancois0@gmail.com>
2022-11-23 15:12:18 +0100

    add list clients & txtbox

---
+++ GestionCommerciale/DeclicInfoGUI/frmConfirmationSuppressionProduit.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace DeclicInfoGUI
+{
+    partial class frmConfirmationSuppressionProduit
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnQuitter = new System.Windows.Forms.Button();
+            this.btnOui = new System.Windows.Forms.Button();
+            this.lblTxt = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnQuitter
+            // 
+            this.btnQuitter.Location = new System.Drawing.Point(153, 267);
+            this.btnQuitter.Name = "btnQuitter";
+            this.btnQuitter.Size = new System.Drawing.Size(75, 23);
+            this.btnQuitter.TabIndex = 0;
+            this.btnQuitter.Text = "Quitter";
+            this.btnQuitter.UseVisualStyleBackColor = true;
+            this.btnQuitter.Click += new System.EventHandler(this.btnQuitter_Click);
+            // 
+            // btnOui
+            // 
+            this.btnOui.Location = new System.Drawing.Point(350, 267);
+            this.btnOui.Name = "btnOui";
+            this.btnOui.Size = new System.Drawing.Size(75, 23);
+            this.btnOui.TabIndex = 1;
+            this.btnOui.Text = "Oui";
+            this.btnOui.UseVisualStyleBackColor = true;
+            this.btnOui.Click += new System.EventHandler(this.btnOui_Click);
+            // 
+            // lblTxt
+            // 
+            this.lblTxt.AutoSize = true;
+            this.lblTxt.Location = new System.Drawing.Point(187, 97);
+            this.lblTxt.Name = "lblTxt";
+            this.lblTxt.Size = new System.Drawing.Size(212, 13);
+            this.lblTxt.TabIndex = 2;
+            this.lblTxt.Text = "Voulez vous vraiment supprimer ce produit?";
+            // 
+            // frmConfirmationSuppressionProduit
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(600, 400);
+            this.Controls.Add(this.lblTxt);
+            this.Controls.Add(this.btnOui);
+            this.Controls.Add(this.btnQuitter);
+            this.Name = "frmConfirmationSuppressionProduit";
+            this.Text = "frmConfirmationSuppressionProduit";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnQuitter;
+        private System.Windows.Forms.Button btnOui;
+        private System.Windows.Forms.Label lblTxt;
+    }
+}
---
+++ GestionCommerciale/DeclicInfoGUI/frmConfirmationSuppressionProduit.cs
@@ -0,0 +1,37 @@
+﻿using DeclicInfoBLL;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DeclicInfoGUI
+{
+    public partial class frmConfirmationSuppressionProduit : Form
+    {
+        string code { get; set; }
+
+        public frmConfirmationSuppressionProduit( string Code)
+        {
+            InitializeComponent();
+            code = Code;
+        }
+
+        private void btnQuitter_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void btnOui_Click(object sender, EventArgs e)
+        {
+            ProduitBLL.DeleteProduit(code);
+            Hide();
+            frmProduits frmproduits = new frmProduits();
+            frmproduits.ShowDialog();
+        }
+    }
+}
